c77d2bf370ac058dd37bb44aea4fa4ed9535c9d5
Phil Kunz <gitlab@philkunz.com>
2019-08-31 16:20:56 +0200

    fix(core): update

--- test/test_npmci.sh
+++ test/test_npmci.sh
@@ -1,9 +1,6 @@
 # check if npmci is available
 npmci -v
 
-# check if we can use lts
-npmci install lts
-
 # check if npm picks it up
-npmci command npm install -g @gitzone/tsrun
-npmci command tsrun -v
+# npmci command npm install -g @gitzone/tsrun
+# npmci command tsrun -v
